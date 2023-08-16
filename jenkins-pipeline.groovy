35d4afddfa79b162de77fbbbd30831b7a75f64c9
madhav2903 <141008318+madhav2903@users.noreply.github.com>
2023-08-16 10:49:32 +0530

    Update jenkins-pipeline.groovy

--- jenkins-pipeline.groovy
+++ jenkins-pipeline.groovy
@@ -1,1 +1,47 @@
+pipeline {
 
+    agent any
+
+    tools {
+
+        nodejs 'v10.19.0'
+
+    }
+
+    stages {
+
+        stage('check'){
+
+            steps {
+
+        git credentialsId: 'Github', url: 'https://github.com/madhav2903/Ant-Design.git'
+
+            }
+
+        }
+
+        stage('build'){
+
+            steps {
+
+                sh 'npm install'
+
+            }
+
+        }
+
+        stage('executeSonarqubeReport')
+
+        {
+
+            steps {
+
+                sh "npm run sonar"
+
+            }
+
+        }
+
+    }
+
+}
